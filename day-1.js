744e77bd4e30307a9bc8290d25f7bf99b2928d71
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-05-09 23:51:33 +0530

    added

--- day-1.js
+++ day-1.js
@@ -1,5 +1,34 @@
-console.log("hii zakya u can do this");
+// console.log("hii zakya u can do this");
 
-console.log(
-  "zakya u r just few steps away from everythning , just believe in urself",
-);
+// console.log(
+//   "zakya u r just few steps away from everythning , just believe in urself",
+// );
+
+////
+
+// 1-to String()
+// 2-join()
+///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+// 1-to String()
+// syntax--- Array.toString()
+// return string with array values separed tby columns
+
+// a)
+// let bikes = ["honda", "yamaha", "suzuki"];
+// console.log(bikes.toString());
+
+// [1, 2, 3].toString()
+
+// let a = ["a", "break", "c"];
+// console.log(a.toString());
+
+// let a = ["hii zakya"];*********************
+// console.log(a.toString());
+
+// let a = [null, undefined];*******************
+// console.log(a.toString());
+
+// console.log([1, "two", 3].toString());
+
+///read-
